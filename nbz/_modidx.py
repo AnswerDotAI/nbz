acd39765a3d8e05a73480c1bd86908a2d272309e
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-02-14 22:30:38 +0000

    Fix command and add spinner

--- nbz/_modidx.py
+++ nbz/_modidx.py
@@ -5,6 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://pydanny.github.io',
                 'git_url': 'https://github.com/pydanny/nbz',
                 'lib_path': 'nbz'},
-  'syms': { 'nbz.core': { 'nbz.core.add_command': ('core.html#add_command', 'nbz/core.py'),
-                          'nbz.core.add_nyi_command': ('core.html#add_nyi_command', 'nbz/core.py'),
-                          'nbz.core.helper': ('core.html#helper', 'nbz/core.py')}}}
+  'syms': { 'nbz.core': { 'nbz.core.add_nyi_command': ('core.html#add_nyi_command', 'nbz/core.py'),
+                          'nbz.core.helper': ('core.html#helper', 'nbz/core.py'),
+                          'nbz.core.with_spinner': ('core.html#with_spinner', 'nbz/core.py')}}}
